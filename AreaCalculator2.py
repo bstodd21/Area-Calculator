a69d90f51d1055870144620669fd048599c0988f
bstodd21 <bstodd20@byui.edu>
2022-05-04 21:03:09 -0700

    area calculator assignment

---
+++ AreaCalculator2.py
@@ -0,0 +1,12 @@
+side = float(input("What is the length of a side of the square? "))
+area = side ** 2
+print(f"The area of the square is: {area}")
+
+length = float(input("What is the length of the rectangle? "))
+width = float(input("What is the width of the rectangle? "))
+area = length * width
+print(f"The area of the rectangle is: {area}")
+
+radius = float(input("What is the radius of the circle? "))
+area = 3.14 * (radius ** 2)
+print(f"The area of the circle is: {area}")
